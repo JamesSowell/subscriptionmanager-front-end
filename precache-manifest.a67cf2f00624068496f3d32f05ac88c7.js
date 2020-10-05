c06def8cdd14a6f01c3d6450160bba5f381151bb
JamesSowell <145jimmi@gmail.com>
2020-10-04 22:25:36 -0700

    Updates

---
+++ precache-manifest.a67cf2f00624068496f3d32f05ac88c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d447310b2cb0cf2f54d72b0d3bf18a",
+    "url": "/subscriptionmanager-front-end/index.html"
+  },
+  {
+    "revision": "56522816811e307bcdec",
+    "url": "/subscriptionmanager-front-end/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d027527a42deb54da9e2",
+    "url": "/subscriptionmanager-front-end/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56522816811e307bcdec",
+    "url": "/subscriptionmanager-front-end/static/js/2.7ec9bdcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/subscriptionmanager-front-end/static/js/2.7ec9bdcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d027527a42deb54da9e2",
+    "url": "/subscriptionmanager-front-end/static/js/main.156ccda3.chunk.js"
+  },
+  {
+    "revision": "215c8c52a7fcde653824",
+    "url": "/subscriptionmanager-front-end/static/js/runtime-main.76d2cf80.js"
+  }
+]);
